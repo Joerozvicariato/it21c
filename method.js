6f3d6bc1a4a20d660f0700edad35d80e363244ab
Joerozvicariato <joerozvicariato287@gmail.com>
2024-10-07 18:52:28 +0800

    Added the console process

--- method.js
+++ method.js
@@ -3,3 +3,10 @@ const person ={
     age:20,
     status:"oks lang"
 }
+
+  //Process and Output
+  console.log("Art Registration Details");
+  console.log("Name: " + art.name);
+  console.log("Type: "  +art.type);
+  console.log("Entry No.: " + art.entry);  
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~");
