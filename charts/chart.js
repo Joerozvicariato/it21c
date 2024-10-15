e821edebb77bf1d3c7b3f3cec971664d52305aa6
Joerozvicariato <joerozvicariato287@gmail.com>
2024-10-16 05:45:44 +0800

    Added barchart barchart because it not show the chartline output

--- charts/chart.js
+++ charts/chart.js
@@ -88,3 +88,40 @@ class LineChart extends ChartCreator {
 }
 const lineChartCreator = new LineChart('data.json');
 lineChartCreator.init(); 
+class BarChart extends ChartCreator {
+    constructor(dataUrl) {
+        super(dataUrl);
+        this.barCtx = document.getElementById('barChart');
+    }
+
+    createCharts() {
+        this.createBarChart();
+    }
+
+    createBarChart() {
+        new Chart(this.barCtx, {
+            type: 'bar',
+            data: {
+                labels: this.chartData.labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: this.chartData.data,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
+
+const barChartCreator = new BarChart('data.json');
+barChartCreator.init();
+
+console.log(lineChartCreator.dataUrl);
+console.log(barChartCreator.dataUrl);
