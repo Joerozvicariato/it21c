970f43e4353d41469e753835fb11a3b6dbac1ffe
Joerozvicariato <joerozvicariato287@gmail.com>
2024-10-15 18:56:01 +0800

    Added async method

--- charts/chart.js
+++ charts/chart.js
@@ -2,5 +2,11 @@ class ChartCreator {
     constructor(dataUrl) {
         this.dataUrl = dataUrl;
         this.chartData = null;
-}
+    }
+    async init() {
+        await this.fetchData();
+        if (this.chartData) {
+            this.createCharts();
+        }
+    }
 }
