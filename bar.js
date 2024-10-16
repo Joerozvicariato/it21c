41277721bdd55c99783e6e3934179616075e86fa
joerozvicariato <joerozvicariato287@gmail.com>
2024-10-16 13:55:04 +0800

    Added all methods on bar.js

--- bar.js
+++ bar.js
@@ -3,4 +3,25 @@ class ChartCreator {
         this.dataUrl = dataUrl;
         this.chartData = null;
     }
+
+async init() {
+    await this.fetchData();
+    if (this.chartData) {
+        this.createCharts();
+    }
+}
+async fetchData() {
+    try {
+        const response = await fetch(this.dataUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        this.chartData = await response.json();
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+createCharts() {
+    throw new Error('createCharts() must be implemented in subclasses');
+}
 }
