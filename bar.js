10b9ae375412a08621fe31daeb062a05258c3347
joerozvicariato <joerozvicariato287@gmail.com>
2024-10-16 13:37:32 +0800

    Added multiple methods barcharts

--- bar.js
+++ bar.js
@@ -3,4 +3,30 @@ class BarChart extends ChartCreator {
         super(dataUrl);
         this.barCtx = document.getElementById('barChart');
     }
+
+createCharts() {
+    this.createBarChart();
 }
+
+createBarChart() {
+    new Chart(this.barCtx, {
+        type: 'bar',
+        data: {
+            labels: this.chartData.labels,
+            datasets: [{
+                label: '# of Votes',
+                data: this.chartData.data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+}
+
